02cfb95ef10638e002195fcdcf6f8b0d84373f47
flavanols <trungnguyen811402@gmail.com>
2026-03-31 22:58:40 +0700

    implement authentication api

--- src/main/java/com/flavanols/identity_service/exception/ErrorCode.java
+++ src/main/java/com/flavanols/identity_service/exception/ErrorCode.java
@@ -1,11 +1,12 @@
 package com.flavanols.identity_service.exception;
 
 public enum ErrorCode {
-    UNCATEGORIZED_EXCEPTION(9999,"Uncategorized error"),
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error"),
     USER_EXISTED(1001, "User existed"),
-    INVALID_KEY(1001,"Invalid message key"),
-    USERNAME_INVALID(1003,"Username must be at least 3 characters"),
-    INVALID_PASSWORD(1004,"Password must be at least 8 characters");
+    USER_NOT_EXISTED(1005, "User not existed"),
+    INVALID_KEY(1001, "Invalid message key"),
+    USERNAME_INVALID(1003, "Username must be at least 3 characters"),
+    INVALID_PASSWORD(1004, "Password must be at least 8 characters");
 
     private int code;
     private String message;
